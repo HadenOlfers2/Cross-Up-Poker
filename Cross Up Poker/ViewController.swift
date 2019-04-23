7a64b01b8571858bab004fea9af5ecc8a9c5123a
HadenOlfers2 <haden.olfers.928@k12.friscoisd.org>
2019-04-23 09:39:55 -0500

    Initial Commit

---
+++ Cross Up Poker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cross Up Poker
+//
+//  Created by Olfers, Haden W on 4/23/19.
+//  Copyright © 2019 Olfers, Haden W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

986e9a09e1e7a0c9a221ade8b24473fb3880cd7b
HadenOlfers2 <haden.olfers.928@k12.friscoisd.org>
2019-04-23 10:12:23 -0500

    Card File & Storyboard Default

---
+++ Cross Up Poker/Card .swift
@@ -0,0 +1,19 @@
+//
+//  Card .swift
+//  Cross Up Poker
+//
+//  Created by Olfers, Haden W on 4/23/19.
+//  Copyright © 2019 Olfers, Haden W. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    let suit: String
+    let number: String
+    
+    init(suit: String, number: String){
+        self.suit = suit
+        self.number = number
+    }
+}

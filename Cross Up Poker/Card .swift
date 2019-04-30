2f0661c4fbcb18026f88c0f490dce1732a09df3d
HadenOlfers2 <haden.olfers.928@k12.friscoisd.org>
2019-04-30 10:15:17 -0500

    Stand and rearrangement

--- Cross Up Poker/Card .swift
+++ Cross Up Poker/Card .swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 struct Card {
     let suit: Suit
--- Cross Up Poker/Deck.swift
+++ Cross Up Poker/Deck.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit 
 
 let Deck = [twoclubs,twodiamonds,twohearts,twospades,threeclubs,threediamonds,threehearts,threespades,fourclubs,Fourdiamonds,fourhearts,fourspades,Fiveclubs,Fivediamonds,Fivehearts,Fivespades,Sixclubs,Sixdiamonds,Sixhearts,Sixspades,Sevenclubs,Sevendiamonds,Sevenhearts,Sevenspades,Eightclubs,Eightdiamonds,Eighthearts,Eightspades,Nineclubs,Ninediamonds,Ninehearts,Ninespades,Tenclubs,Tendiamonds,Tenhearts,Tenspades,Jackclubs,Jackdiamonds,Jackhearts,Jackspades,Queenclubs,Queendiamonds,Queenhearts,Queenspades,Kingclubs,Kingdiamonds,Kinghearts,Kingspades,Aceclubs,Acediamonds,Acehearts,Acespades]
 

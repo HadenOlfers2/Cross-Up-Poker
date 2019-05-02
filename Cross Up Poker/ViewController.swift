ad0f23b95d2e6de338cd10641730f418cd9557e0
HadenOlfers2 <haden.olfers.928@k12.friscoisd.org>
2019-05-02 10:15:28 -0500

    CollectionView for Deck

--- Cross Up Poker/ViewController.swift
+++ Cross Up Poker/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var Stand: UICollectionView!
+    @IBOutlet weak var Deck: UICollectionView!
     
     @IBOutlet weak var twoclubs: UIImageView!
     @IBOutlet weak var twodiamonds: UIImageView!
